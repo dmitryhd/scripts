e49d5ea7ad6b065ae0c2df09ed547decc94845ae
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-03-05 08:22:08 +0300

    lock created

--- python_concept/sqlalchemy_lock.py
+++ python_concept/sqlalchemy_lock.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-import sqlalchemy
-
-print('hello, alchemy')
-
 from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy.sql import select
 
-engine = create_engine('mysql+pymysql://root:111111@localhost/foo', echo=True)
-
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+engine = create_engine('mysql+pymysql://root:111111@localhost/foo', echo=True, echo_pool=True)
 
 metadata = MetaData()
 
@@ -18,6 +14,16 @@ users = Table('users', metadata,
      Column('fullname', String(12)),
  )
 
-metadata.create_all(engine) 
+metadata.create_all(engine)
+
+conn = engine.connect()
+
+ins = users.insert().values(name='jack', fullname='Jack Jones')
+result = conn.execute(ins)
+
+s = select([users])
+result = conn.execute(s)
+for row in result:
+    print(row)
 
 engine.execute('drop database foo;')
