cd5ed94900d9c7c763d76e71718392b78fcf9e6a
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-06-13 08:12:00 +0300

    work with nginx start_profiler() end_profiler()
    timer_b = None
    
    def start_profiler():
        import time;
        global timer_b
        timer_b = time.time();
    
    def end_profiler(message = 'TIMING: '):
        from colorama import init, Fore;
        init();
        print(message, Fore.YELLOW , round(time.time() - timer_b, 3), Fore.RESET)
    
    import argparse
    
    def parse_args():
        """ Process command line arguments. """
        parser = argparse.ArgumentParser()
        parser.add_argument("-d", "--db_name", type=str,
                            default='data/vac.db',
                            help="database name")
        parser.add_argument("-c", "--compress",
                            help="do compression",
                            action="store_true")
        args = parser.parse_args()
        if args.compress:
            args.db_name = 'xxx'
        return args
    
    def main():
        """ """
        pass
    
    if __name__ == '__main__':
        main()
    
    import unittest
    
    class TestS(unittest.TestCase):
        def test_smth(self):
            self.assertTrue(True)
    
    if __name__ == '__main__':
        unittest.main()

--- html5_example/uwsgirun.sh
+++ html5_example/uwsgirun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #uwsgi --socket 127.0.0.1:3031 -w flask-hw
-uwsgi --http :8000 --module flaskhw --callable app
-#uwsgi --socket 127.0.0.1:3031 --module flaskhw --callable app
+#uwsgi --http :8000 --module flaskhw --callable app
+uwsgi --socket 127.0.0.1:3031 --module flaskhw --callable app
