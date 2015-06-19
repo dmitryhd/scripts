a4ab22d9cd837958160e9a6aaab92967ae96fe00
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-06-19 10:15:00 +0300

    lua modules dont work start_profiler() end_profiler()
    timer_b = None
    
    def start_profiler():
        import time;
        global timer_b
        timer_b = time.time();
    
    def end_profiler(message = 'TIMING: '):
        from colorama import init, Fore;
        init();
        print(message, Fore.YELLOW , round(time.time() - timer_b, 3), Fore.RESET)
    
    import argparse
    
    def parse_args():
        """ Process command line arguments. """
        parser = argparse.ArgumentParser()
        parser.add_argument("-d", "--db_name", type=str,
                            default='data/vac.db',
                            help="database name")
        parser.add_argument("-c", "--compress",
                            help="do compression",
                            action="store_true")
        args = parser.parse_args()
        if args.compress:
            args.db_name = 'xxx'
        return args
    
    def main():
        """ """
        pass
    
    if __name__ == '__main__':
        main()
    
    import unittest
    
    class TestS(unittest.TestCase):
        def test_smth(self):
            self.assertTrue(True)
    
    if __name__ == '__main__':
        unittest.main()

--- lua/show_dirs.lua
+++ lua/show_dirs.lua
@@ -1,5 +1,33 @@
--- prints all directories in given root directory
+#!/usr/bin/env lua
 
+-- Prints all directories in given root directory.
+
+lua = require("lfs")
+--path = '/usr/share/lua/5.1/lfs.so'
+--lfs = package.loadlib(path, "luaopen_socket")
 root = '/home/dmitryhd/.config/awesome/themes/'
 
+
+function isdir(fn)
+    return (lfs.attributes(fn,"mode")== "directory")
+end
+
+
+-- Lua implementation of scandir function
+function scandir(directory)
+    local i, t, popen = 0, {}, io.popen
+    for filename in popen('ls -a "'..directory..'"'):lines() do
+        i = i + 1
+        t[i] = filename
+    end
+    return t
+end
+
+
 print(root)
+dirs = scandir(root)
+for k, v in pairs( dirs ) do
+    if isdir(root .. v) then
+        print(k, v)
+    end
+end
